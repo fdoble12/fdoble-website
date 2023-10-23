264217cec40106c43258280d4b0c364c4a891fad
francisdoble <francis_doble@dlsu.edu.ph>
2023-10-23 10:40:59 +0800

    fixed hero nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,18 @@
 <template>
     <nav class="bg-[#303030] p-4 fixed w-full">
-        <div class="container mx-auto flex justify-between items-center">
-            <div class="text-white text-2xl font-semibold">Francis Doble</div>
-            <div class="space-x-4">
-                <a href="#about" class="text-white hover:text-blue-200">About</a>
-                <a href="#projects" class="text-white hover:text-blue-200">Projects</a>
-                <a href="#contact" class="text-white hover:text-blue-200">Contact</a>
-                <button class="bg-blue-700 text-white px-4 py-2 rounded-full hover:bg-blue-600">Download CV</button>
-            </div>
+      <div class="container mx-auto flex flex-wrap justify-between items-center">
+        <div class="text-white text-2xl md:text-3xl lg:text-4xl font-semibold mb-2 md:mb-0">
+          Francis Doble
         </div>
+        <div class="md:flex space-x-4 md:space-x-6">
+          <a href="#about" class="text-white hover:text-blue-200">About</a>
+          <a href="#projects" class="text-white hover:text-blue-200">Projects</a>
+          <a href="#contact" class="text-white hover:text-blue-200">Contact</a>
+          <button class="bg-blue-700 text-white px-4 py-2 rounded-full hover:bg-blue-600">
+            Download CV
+          </button>
+        </div>
+      </div>
     </nav>
-</template>
-
-<script>
-    export default {
-    name: 'Navbar',
-    };
-</script>
+  </template>
+  
