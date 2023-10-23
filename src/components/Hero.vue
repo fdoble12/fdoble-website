1fd0d47f152394de9c31f5bf449f39b26ed4432b
francisdoble <francis_doble@dlsu.edu.ph>
2023-10-23 11:12:11 +0800

    add transition

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,20 +1,16 @@
 <template>
-  <div class="bg-black text-white min-h-screen flex items-center justify-center">
-    <div class="w-full mx-4 md:flex md:items-center md:justify-between">
-      <!-- Left: Large Text -->
-      <div class="md:w-1/2 p-8 pl-4">
-        <h1 class="text-2xl md:text-4xl lg:text-6xl font-extrabold">
-          FRANCIS DOBLE
-        </h1>
-      </div>
+  <div class="bg-black text-white min-h-screen flex flex-col items-center justify-center">
+    <!-- Top: Large Text with Animation -->
+    <h1 class="text-2xl md:text-4xl lg:text-[100px] font-extrabold text-center m-4 p-8 falling-text">
+      FRANCIS DOBLE
+    </h1>
 
-      <!-- Right: Editable Square Placeholder -->
-      <div class="md:w-1/2 p-8">
-        <div class="bg-blue-500 p-6 rounded-lg">
-          <!-- Placeholder for your introduction -->
-          <div class="p-2 rounded-md bg-gray-100 text-gray-800 text-justify">
-            I am Francis Doble, a 4th-year Computer Science student at De La Salle University-Manila. I am a goal-oriented and eager individual with leadership skills developed through my co-curricular activities. I have gained practical experience in various areas, including data analysis, data engineering, project management, software testing and development, and automation, through multiple internships. Additionally, I have acquired technical proficiency in Azure, Microsoft Power Tools, Python, JavaScript, Vue.js, Tailwind CSS, and database management tools like MongoDB and MySQL. My curiosity drives me to constantly learn and improve my skills. I'm always excited to take on new challenges and work on innovative projects.
-          </div>
+    <!-- Bottom: Editable Introduction with Animation -->
+    <div class="w-full md:w-1/2 p-4 falling-text">
+      <div class="bg-blue-500 p-6 rounded-lg">
+        <!-- Placeholder for your introduction -->
+        <div class="p-2 rounded-md bg-gray-100 text-gray-800 text-justify">
+          I am Francis Doble, a 4th-year Computer Science student at De La Salle University-Manila. I am a goal-oriented and eager individual with leadership skills developed through my co-curricular activities. I have gained practical experience in various areas, including data analysis, data engineering, project management, software testing and development, and automation, through multiple internships. Additionally, I have acquired technical proficiency in Azure, Microsoft Power Tools, Python, JavaScript, Vue.js, Tailwind CSS, and database management tools like MongoDB and MySQL. My curiosity drives me to constantly learn and improve my skills. I'm always excited to take on new challenges and work on innovative projects.
         </div>
       </div>
     </div>
@@ -26,3 +22,22 @@ export default {
   name: 'Hero',
 };
 </script>
+
+<style scoped>
+/* Define the animation effect */
+@keyframes falling {
+  0% {
+    transform: translateY(-100%);
+    opacity: 0;
+  }
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+/* Apply the animation to both text and the introduction */
+.falling-text {
+  animation: falling 1s ease forwards;
+}
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -4,7 +4,7 @@
         <div class="text-white text-2xl md:text-3xl lg:text-4xl font-semibold mb-2 md:mb-0">
           Francis Doble
         </div>
-        <div class="md:flex space-x-4 md:space-x-6">
+        <div class="md:flex items-center space-x-4 md:space-x-6">
           <a href="#about" class="text-white hover:text-blue-200">About</a>
           <a href="#projects" class="text-white hover:text-blue-200">Projects</a>
           <a href="#contact" class="text-white hover:text-blue-200">Contact</a>
