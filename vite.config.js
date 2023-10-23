b3d5572efc606e0397bf1909fe25deec54a73614
francisdoble <francis_doble@dlsu.edu.ph>
2023-10-23 09:56:09 +0800

    change base path

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const basePath = 'https://fdoble12.github.io/fdoble-website/';
+const basePath = '/fdoble-website/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    base: basePath,
-  }
+  base: basePath
 })
