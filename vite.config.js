e4b9a6f4b4be3ff23c9be6bdef879766ee9f5238
francisdoble <francis_doble@dlsu.edu.ph>
2024-01-14 23:24:44 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const basePath = '/fdoble-website/';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-    base: basePath
+  plugins: [vue()]
 })
