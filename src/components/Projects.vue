d796e5fafefad17eb1cc2ce17816d3804c122eed
francisdoble <francis_doble@dlsu.edu.ph>
2024-01-14 23:52:52 +0800

    Update Projects.vue

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,7 +2,10 @@
     <section id="projects" class="bg-violet-400 border-2 border-black py-16 min-h-[70vh]">
       <div class="container mx-auto">
         <h2 class="text-3xl md:text-4xl text-black font-semibold mb-4">Projects</h2>
-        <div class="bg-white p-4 rounded-lg shadow-md border-2 border-black">
+  
+        <!-- Project Cards -->
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div class="bg-white p-4 rounded-lg shadow-md border-2 border-black">
             <div class="mb-4">
               <img
                 src="../assets/filcrest.png"
@@ -13,8 +16,6 @@
             <h3 class="text-xl text-blue-600 underline font-semibold mb-2"><a href="https://filcrestrealty.vercel.app/" class="hover:text-blue-800"> Real Estate Website </a></h3>
             <p class="text-gray-700 text-justify">A real estate website that shows details about a real estate firm and its featured listings. <span class="font-bold">The website was created using Vue.js, Vue router, and Tailwind CSS.</span></p>
           </div>
-        <!-- Project Cards -->
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <!-- Project Card 1 -->
           <div class="bg-white p-4 rounded-lg shadow-md border-2 border-black">
             <div class="mb-4">
@@ -26,19 +27,6 @@
             </div>
             <h3 class="text-xl text-blue-600 underline font-semibold mb-2"><a href="https://github.com/nicapos/discussion-forum" class="hover:text-blue-800"> Disqussip </a></h3>
             <p class="text-gray-700 text-justify">A web application that allows users to create and participate in discussion forums. <span class="font-bold">Built using Node.js (Express), Handlebars, and MongoDB</span>, that handled the user interface, API calls, and data storage of the application.</p>
-          </div>
-          
-          <!-- Project Card 2 -->
-          <div class="bg-white p-4 rounded-lg shadow-md border-2 border-black">
-            <div class="mb-4">
-              <img
-                src="../assets/lashout.png"
-                alt="Project 2"
-                class="w-full h-80 object-cover rounded border-2 border-black"
-              />
-            </div>
-            <h3 class="text-xl underline text-blue-600 font-semibold mb-2"><a href="https://github.com/leimons/LashOutMNL" class="hover:text-blue-800"> LashoutMnl </a></h3>
-            <p class="text-gray-700 text-justify">Lash Out MNL Beauty Lounge is a beauty services parlor that operates at BF Homes Almanza, Las Pinas City, Philippines. This web application is <span class="font-bold">created using Vue, NodeJS Express, and MongoDB</span> to handle the booking services of the lash studio.</p>
           </div>
 
           <div class="bg-white p-4 rounded-lg shadow-md border-2 border-black">
@@ -54,6 +42,17 @@
           </div>
 
           <!-- Project Card 2 -->
+          <div class="bg-white p-4 rounded-lg shadow-md border-2 border-black">
+            <div class="mb-4">
+              <img
+                src="../assets/lashout.png"
+                alt="Project 2"
+                class="w-full h-80 object-cover rounded border-2 border-black"
+              />
+            </div>
+            <h3 class="text-xl underline text-blue-600 font-semibold mb-2"><a href="https://github.com/leimons/LashOutMNL" class="hover:text-blue-800"> LashoutMnl </a></h3>
+            <p class="text-gray-700 text-justify">Lash Out MNL Beauty Lounge is a beauty services parlor that operates at BF Homes Almanza, Las Pinas City, Philippines. This web application is <span class="font-bold">created using Vue, NodeJS Express, and MongoDB</span> to handle the booking services of the lash studio.</p>
+          </div>
 
           <!-- Project Card 3 -->
           <div class="bg-white p-4 rounded-lg shadow-md border-2 border-black">
