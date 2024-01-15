33358b0ed9ce827f2002f90f4dfe635f44db74f5
francisdoble <francis_doble@dlsu.edu.ph>
2024-01-15 08:19:42 +0800

    delete build and path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const basePath = '/fdoble-website/';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: basePath,
   plugins: [vue()]
 })
