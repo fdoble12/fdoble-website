acd773b93c5d573af5b1464c949d27506428c569
francisdoble <francis_doble@dlsu.edu.ph>
2024-01-15 08:18:23 +0800

    update

--- src/components/Cocurriculars.vue
+++ src/components/Cocurriculars.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="cocurriculars" class="bg-gray-100 border-2 border-black py-8">
-    <div class="container mx-auto min-h-[60vh]">
-      <h2 class="text-3xl font-semibold text-gray-800 mb-6">Co-curricular Experiences</h2>
+    <div class="container mx-auto min-h-[60vh] px-6">
+      <h2 class="text-xl md:text-3xl font-semibold text-gray-800 mb-4 md:mb-6">Co-curricular Experiences</h2>
 
       <div class="flex items-center justify-center mx-auto">
         <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4">
           <div v-for="(experience, index) in experiences" :key="index">
-            <div class="bg-white p-4 rounded-lg shadow-md flex flex-col sm:flex-row border-2 border-black">
-              <div class="sm:w-2/5 pr-4">
+            <div class="bg-white p-4 rounded-lg shadow-md flex flex-col justify-center items-center sm:flex-row border-2 border-black">
+              <div class="mb-2">
                 <img v-bind:src="experience.image" class="w-full h-full">
               </div>
               <div class="sm:w-3/5">
-                <h3 class="text-2xl font-semibold text-gray-800 mb-2">{{ experience.title }}</h3>
-                <p class="text-gray-700 text-justify">{{ experience.description }}</p>
+                <h3 class="text-lg md:text-2xl font-semibold text-gray-800 mb-1 md:mb-2">{{ experience.title }}</h3>
+                <p class="text-gray-700 text-justify text-sm md:text-base">{{ experience.description }}</p>
               </div>
             </div>
           </div>
