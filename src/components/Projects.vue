7c7a88b81146572e7c16cd54a96e3bd2d6ddf5ec
francisdoble <francis_doble@dlsu.edu.ph>
2024-01-23 14:55:53 +0800

    Update Projects.vue

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -72,42 +72,42 @@
           {
             title: "Real Estate Website",
             description: "A real estate website that shows details of a real estate firm and its featured listings.",
-            img: "/projects/filcrest.png",
+            img: "./projects/filcrest.png",
             url: "https://filcrestrealty.vercel.app/",
             skills: ["HTML","CSS","JavaScript","VueJS","TailwindCSS"]
           },
           {
             title: "Disqussip",
             description: "A web application that allows users to create and participate in discussion forums.",
-            img: "/projects/disqussip.png",
+            img: "./projects/disqussip.png",
             url: "https://github.com/nicapos/discussion-forum",
             skills: ["HTML","CSS","JavaScript","Node.js","MongoDB"]
           },
           {
             title: "Sentiment Analysis Web Application",
             description: "A simple web application that applies sentiment analysis on comments from YouTube videos.",
-            img: "/projects/yt-sent.png",
+            img: "./projects/yt-sent.png",
             url: "https://github.com/fdoble12/yt-sentiments-webapp",
             skills: ["VueJS","Python","Flask","Text Classifier","Machine Learning"]
           },
           {
             title: "Employee Index Dashboard",
             description: "A simple PowerBI dashboard that visualizes an employee index dataset from Kaggle",
-            img: "/projects/pbi.png",
+            img: "./projects/pbi.png",
             url: "https://app.powerbi.com/view?r=eyJrIjoiYmUzYWVhMzYtOWQzMC00M2QyLWI4ZWYtZjJlMTkxNWU0MTk5IiwidCI6ImYzNGEzNWJkLWE2NWQtNDYwNS1iMGZhLWQyNTcxZjgzMWY1ZSIsImMiOjEwfQ%3D%3D",
             skills: ["Microsoft PowerBI","Data Analysis & Visualization"]
           },
           {
             title: "LashoutMnl",
             description: "This web application handles the booking services of LashOut MNL studio.",
-            img: "/projects/lashout.png",
+            img: "./projects/lashout.png",
             url: "https://github.com/leimons/LashOutMNL",
             skills: ["VueJS","Node.js","MongoDB"]
           },
           {
             title: "Coffee Ordering Android Application",
             description: "Users are able to choose and place an order, as well as selecting their delivery address.",
-            img: "/projects/yc.png",
+            img: "./projects/yc.png",
             url: "https://github.com/donnielle-andres/MOBDEVE-MCO",
             skills:["Google API", "Kotlin"]
           }
